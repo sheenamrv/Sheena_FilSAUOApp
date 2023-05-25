f9d789e912b135cf74a9e76a0db3387f21570c43
Sheena Verana <67240854+sheenamrv@users.noreply.github.com>
2023-05-25 08:17:42 -0400

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { EventsComponent } from './events/events.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { TeamComponent } from './team/team.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'about-us', component: AboutUsComponent},
+  {path:'events', component: EventsComponent},
+  {path:'team', component: TeamComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'header', component: HeaderComponent},
+  {path:'footer', component: FooterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [HomeComponent, AboutUsComponent, EventsComponent, TeamComponent, RegisterComponent, HeaderComponent, FooterComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { HomeComponent } from './home/home.component';
+// import { HeaderComponent } from './header/header.component';
+// import { FooterComponent } from './footer/footer.component';
+// import { AboutUsComponent } from './about-us/about-us.component';
+// import { EventsComponent } from './events/events.component';
+// import { TeamComponent } from './team/team.component';
+// import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
