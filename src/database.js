74dbc91868a2b0e6c5108dc1bb3d1464c200bf74
Sheena Verana <67240854+sheenamrv@users.noreply.github.com>
2023-06-05 14:57:37 -0400

    update database.js

--- src/database.js
+++ src/database.js
@@ -8,6 +8,8 @@ const client = new Client ({
     database: "FilsauoDB"
 })
 
+
+
 client.connect();
 
 client.query (`Select * from events`, (err, res) => {
@@ -19,3 +21,75 @@ client.query (`Select * from events`, (err, res) => {
 
   client.end
 })
+
+// module.exports = client
+
+// const client = require('./connection.js')
+// const express = require('express');
+// const app = express();
+
+// app.listen(3300, ()=>{
+//     console.log("Sever is now listening at port 3000");
+// })
+
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.json());
+
+// //GET
+// app.get('/events', (req, res)=>{
+//   client.query(`Select * from events`, (err, result)=>{
+//       if(!err){
+//           res.send(result.rows);
+//       }
+//   });
+//   client.end;
+// })
+// client.connect();
+
+
+//POST
+app.post('/events', (req, res)=> {
+  const event = req.body;
+  let insertQuery = `insert into events(id, firstname, lastname, location) 
+                     values(${event.id}, '${event.firstname}', '${event.lastname}', '${event.location}')`
+
+  client.query(insertQuery, (err, result)=>{
+      if(!err){
+          res.send('Insertion was successful')
+      }
+      else{ console.log(err.message) }
+  })
+  client.end;
+})
+
+// //PUT
+// app.put('/events/:id', (req, res)=> {
+//   let event = req.body;
+//   let updateQuery = `update events
+//                      set firstname = '${event.firstname}',
+//                      lastname = '${event.lastname}',
+//                      location = '${event.location}'
+//                      where id = ${event.id}`
+
+//   client.query(updateQuery, (err, result)=>{
+//       if(!err){
+//           res.send('Update was successful')
+//       }
+//       else{ console.log(err.message) }
+//   })
+//   client.end;
+// })
+
+
+// //DELETE
+// app.delete('/events/:id', (req, res)=> {
+//   let insertQuery = `delete from events where id=${req.params.id}`
+
+//   client.query(insertQuery, (err, result)=>{
+//       if(!err){
+//           res.send('Deletion was successful')
+//       }
+//       else{ console.log(err.message) }
+//   })
+//   client.end;
+// })
